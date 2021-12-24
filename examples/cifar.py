f36eb3bbc9619670d90aa123d5efdeee694d9ac2
Ha Minh Quan <quan722soi@gmail.com>
2021-12-24 20:33:35 +0700

    commit

--- examples/cifar.py
+++ examples/cifar.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
+import torch.nn as nn
 
 import torchvision
 import torchvision.transforms as transforms
